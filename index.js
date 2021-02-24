281d7717075f6a36d90c22f5409190e580ebb873
seethal585 <seethal.thomas85@gmail.com>
2021-02-24 17:03:35 +1100

    added async-await

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+//API key generated
 const appId = '9548b67bc2f02d4b6eda7880afdac810'
-const getDataForCity = city => fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appId}&units=metric`)
-  .then(response => response.json());
+const goButton = document.querySelector('#go-button')
+const weatherDisplay = document.querySelector('#weather-display')
+const city = document.querySelector('#city')
+//fetching data from API
+// const getDataForCity = city => fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appId}&units=metric`)
+//   .then(response => response.json());
 //   console.log(JSON)
   const createCardHtml = (country, name, emoji, temp, feelsLike, description) =>
 {   console.log('inside createCardHTML')
@@ -49,18 +54,38 @@ const emojis = {
   '13n': '❄️',
   '50n': '💨',
 }
-const goButton = document.querySelector('#go-button')
-const weatherDisplay = document.querySelector('#weather-display')
-const city = document.querySelector('#city')
-goButton.addEventListener('click',() =>
-{
-    console.log('inside goButton')
-    let cityValue = city.value
-    console.log(cityValue)
-    getDataForCity(cityValue) .then(data => {
+
+// goButton.addEventListener('click',() =>
+// {
+//     console.log('inside goButton')
+//     let cityValue = city.value
+//     console.log(cityValue)
+//     getDataForCity(cityValue) .then(data => {
      
-        console.log('inside getdataForCity')
-     const country = data.sys.country;
+//         console.log('inside getdataForCity')
+//      const country = data.sys.country;
+//      console.log(country)
+//       const name = data.name;
+//       console.log(name)
+//       const emoji = emojis[data.weather[0].icon];
+//       const temp = data.main.temp;
+//       const feelsLike = data.main.feels_like;
+//       const description = data.weather[0].main;
+//       console.log(description)
+//     const cardHtml = createCardHtml(country, name, emoji, temp, feelsLike, description);
+//     weatherDisplay.innerHTML = cardHtml
+//     })
+// })
+goButton.addEventListener('click', async () =>
+{
+    console.log('inside click')
+    const cityValue = city.value
+    console.log(city)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${appId}&units=metric`)
+    console.log(response)
+    const data = await response.json()
+    console.log(data)
+    const country = data.sys.country;
      console.log(country)
       const name = data.name;
       console.log(name)
@@ -71,5 +96,5 @@ goButton.addEventListener('click',() =>
       console.log(description)
     const cardHtml = createCardHtml(country, name, emoji, temp, feelsLike, description);
     weatherDisplay.innerHTML = cardHtml
-    })
+
 })
